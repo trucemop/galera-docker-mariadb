0a217f2a2d37b065eb49863150422d05290ecd8a
John Highcock <trucemop@gmail.com>
2022-01-02 22:48:18 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -218,4 +218,4 @@ else
 	export _WSREP_NEW_CLUSTER=''
 fi
 
-su -u mysql -c 'mariadbd --wsrep_cluster_name=$CLUSTER_NAME --wsrep-cluster-address="gcomm://$cluster_join" $_WSREP_NEW_CLUSTER $CMDARG'
+su - mysql -c 'mariadbd --wsrep_cluster_name=$CLUSTER_NAME --wsrep-cluster-address="gcomm://$cluster_join" $_WSREP_NEW_CLUSTER $CMDARG'
